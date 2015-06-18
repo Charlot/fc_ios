8cf9be7a5c1d722b4ac4e1d5c44e6ec35a8c014e
ryan715 <yxr.306789@gmail.com>
2015-06-18 10:17:18 +0800

    yiku  UI

---
+++ Material/Material/YikuViewController.h
@@ -0,0 +1,13 @@
+//
+//  YikuViewController.h
+//  Material
+//
+//  Created by exmooncake on 15-6-18.
+//  Copyright (c) 2015年 brilliantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YikuViewController : UIViewController
+
+@end
---
+++ Material/Material/YikuViewController.m
@@ -0,0 +1,38 @@
+//
+//  YikuViewController.m
+//  Material
+//
+//  Created by exmooncake on 15-6-18.
+//  Copyright (c) 2015年 brilliantech. All rights reserved.
+//
+
+#import "YikuViewController.h"
+
+@interface YikuViewController ()
+
+@end
+
+@implementation YikuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
