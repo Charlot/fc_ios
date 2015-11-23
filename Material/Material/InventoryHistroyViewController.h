33cc2dd597c864922c48edb4f2373a070a06acb3
ryan715 <yxr.306789@gmail.com>
2015-11-23 11:23:00 +0800

    add inventory ui

---
+++ Material/Material/InventoryHistroyViewController.h
@@ -0,0 +1,13 @@
+//
+//  InventoryHistroyViewController.h
+//  Material
+//
+//  Created by ryan on 11/23/15.
+//  Copyright © 2015 brilliantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InventoryHistroyViewController : UIViewController
+
+@end
---
+++ Material/Material/InventoryPositionViewController.h
@@ -0,0 +1,13 @@
+//
+//  InventoryPositionViewController.h
+//  Material
+//
+//  Created by ryan on 11/23/15.
+//  Copyright © 2015 brilliantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InventoryPositionViewController : UIViewController
+
+@end
